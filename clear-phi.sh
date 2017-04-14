0b225372df4eb39feb66168346b182f8959d7a94
Brian Lindsey <brianl9864@gmail.com>
2017-04-14 15:02:06 -0400

    PULSE-421 take user and host out of query

--- clear-phi.sh
+++ clear-phi.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-psql -h localhost -U pulse -f clear-phi.sql pulse
+if [ $# -ne 2 ]; then
+    host=localhost
+    user=pulse
+else
+    host=$1
+    user=$2
+fi
+psql -h $host -U $user -f clear-phi.sql pulse
