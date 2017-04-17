7419e279baba7e242eacabad7f7caae99a4a7e0c
Andrew Larned <andrew.larned@gmail.com>
2017-04-17 07:26:17 -0400

    Update clear-phi.sh

--- clear-phi.sh
+++ clear-phi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+(set -o igncr) 2>/dev/null && set -o igncr; # this comment is required to trick cygwin into dealing with windows vs. linux EOL characters
+
 if [ $# -ne 2 ]; then
     host=localhost
     user=pulse
